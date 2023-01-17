8d4381d2c7652d2676c128bb65b8cf543aa29250
MunzeyDev <67990033+MunzeyDev@users.noreply.github.com>
2023-01-16 20:01:16 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,27 @@
-print("ADDITION = (+) | SUBTRACTION = (-) | MULTIPLICATION = (*) | DIVISION = (/)")
-print("")
-op1 = input("Enter an operator: ")
-num1 = int(input("What is your first number?: "))
-num2 = int(input("What is your second number?: "))
-print("")
-print("Your answer is: ")
-if op1 == "+":
-    print(num1 + num2)
-elif op1 == "-":
-    print(num1 - num2)
-elif op1 == "*":
-    print(num1 * num2)
-elif op1 == "/":
-    print(num1 / num2)
+def main():
+
+    num1 = int(input("What is your first number?: "))
+    print()
+    print("ADDITION = (+) | SUBTRACTION = (-) | MULTIPLICATION = (*) | DIVISION = (/)")
+    op1 = input("Enter an operator: ")
+    num2 = int(input("What is your second number?: "))
+    print("")
+    print("Your answer is: ")
+    if op1 == "+":
+        print(num1 + num2)
+    elif op1 == "-":
+        print(num1 - num2)
+    elif op1 == "*":
+        print(num1 * num2)
+    elif op1 == "/":
+        print(num1 / num2)
+    print("")
+    Repeat = input("Would you like to try it again?").lower()
+    if Repeat == "yes":
+        print()
+        main()
+    else:
+        print("")
+        print("Goodbye")
+        exit()
+main()
